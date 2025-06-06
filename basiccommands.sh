219683685dab850f84cd3bee5f69163ecb1f95af
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-06-06 17:50:13 +0700

    add basiccommands.sh

---
+++ basiccommands.sh
@@ -0,0 +1,22 @@
+echo "
+Bash commands are how you interact with the operating system and performa tasks.
+
+Common commands:
+1. ls - List directory contents
+2. cd - Change the current directory
+3. pwd - Print the current working directory
+4. echo - Display a line of text
+5. cat - Concatenate and display files
+6. cp - Copy files and directories
+7. mv - Move or rename files
+8. rm - Delete files or folders
+9. touch - Create an empty file or update its time
+10. mkdir - Create new folder
+
+example:
+mkdir my_directory
+cd my_directory
+touch my_file.txt
+ls
+my_file.txt
+"
